a14b268bc51eefc448f7bc61875be5ab3896c510
Nishtha <nishtha@Nishthas-MacBook-Pro.local>
2016-09-15 20:25:06 -0500

    few more changes

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -16,8 +16,6 @@ var contentEditable = true;
 // this function will store the data in the firebase database on click of a submit button
 function storingInDatabase() {
 
-    // event.preventDefault();
-
     // get the value from the input fields to set them in the database
     var trainName = $('#trainNameInput').val().trim();
     var destination = $('#destinationInput').val().trim();
@@ -44,19 +42,34 @@ function storingInDatabase() {
 
 // this function is calculation the next train and also minutes remaining for the train to arrive
 function getMinutesTillNextTrain(firstTrainTime, frequency) {
-	// current time
+    var timeRemaining;
+	
+    firstTrainTime = moment(parseInt(firstTrainTime));
+    console.log(firstTrainTime);
+
+    // current time
     var currentTime = moment();
     console.log('current Time' + moment(currentTime).format('HH:mm'));
     // time difference between current time and fir time train arrives
-    var timeDiff = currentTime - firstTrainTime;
+    var timeDiff = currentTime - firstTrainTime
     console.log('time diff btw current and first train time ' + timeDiff);
+
     // changing time difference format in minutes
-    timeDiff = moment(timeDiff).format('mm');
-    // finding modulus of time diff and frequency to check the time remaining for next
-    var timeRemaining = frequency - (timeDiff % frequency);
-    console.log('time remaining ' + timeRemaining);
+    timeDiff = Math.floor( timeDiff / ( 60*1000 ) );
+
+    // if first train is later than current time.
+    if( timeDiff < 0 ) {
+        timeDiff = Math.abs(timeDiff);
+        timeRemaining = timeDiff;
+    } else {
+        // finding modulus of time diff and frequency to check the time remaining for next
+        timeRemaining = frequency - (timeDiff % frequency);
+        // console.log('time remaining ' + timeRemaining);
+    }
 
     return timeRemaining;
+
+
 }
 
 //Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
@@ -68,15 +81,15 @@ database.ref().on('child_added', function(childSnapshot) {
     var firstTrainTime = childSnapshot.val().firstTrainTime;
      //converting first train format in hours and mins 
     var firstTrain = moment(parseInt(firstTrainTime)).format('HH:mm');
-    	console.log('first train' + firstTrain);
+    	// console.log('first train' + firstTrain);
 
     var frequency = childSnapshot.val().frequency;
-    	console.log('train frequency ' + moment(frequency).format('MM'));
+    	// console.log('train frequency ' + moment(frequency).format('MM'));
     
     var minutesTillNextTrain = getMinutesTillNextTrain(firstTrainTime, frequency);
 
     var nextTrain = moment().add(minutesTillNextTrain, 'minutes').format('hh:mm A');
-    	console.log('Next Train:',nextTrain);
+    	// console.log('Next Train:',nextTrain);
 
     $('#trainTable > tbody').append(
         '<tr data-first-train="' + firstTrain +'"><td class="name">' + trainName + '</td>' +
@@ -99,7 +112,8 @@ database.ref().on('child_added', function(childSnapshot) {
 once the data is deleted by the user it will be removed from the database as well*/
 $(document).on('click', '.remove', function(){
 	$(this).parent('tr').remove();
-	database.ref().remove();
+    var trainName = $(this).parent('tr').find('.name').text();
+	database.ref().remove(trainName);
 	
 });
 
@@ -112,7 +126,7 @@ $(document).on('click','.editButton', function(){
 		var frequency = $('.trainFrequency').html();
 		var nextTrain = $('.nextArrival').html();
 		var firstTrainTime = $(this).closest('tr').data('first-train');
-		console.log('test' + firstTrainTime);
+		// console.log('test' + firstTrainTime);
 		var minutesTillNextTrain = $('.nextTrainTime').html();
 		
 		$('#trainNameInput').val(name);
@@ -122,10 +136,22 @@ $(document).on('click','.editButton', function(){
 
 })
 
-// $('#updateButton').on('click', function(){
-// 	if (childSnapshot.child("trainName").exists()
+$('#updateButton').on('click', function(){
+    var test = $('#trainNameInput').val().trim();
 
-// })
+    database.ref().update({
+        trainName: trainName,
+      });      
+    // console.log($('tr').data('first-train'));
+    // console.log('testing:' + test);
+    // var destination = $('#destinationInput').val().trim();
+    // var firstTrainTime = moment($('#firstTrainTimeInput').val(), 'HH:mm').format('x');
+    // var frequency = $('#trainFrequencyInput').val().trim();
+
+              
+
+
+})
 
 
 
